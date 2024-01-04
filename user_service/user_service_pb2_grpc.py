1d1eb490f43d6b99b42d430d084ac6b940b089a5
Alexsei Popov <popov2895@gmail.com>
2024-01-04 15:57:45 +0300

    add get friend stats method

--- user_service/user_service_pb2_grpc.py
+++ user_service/user_service_pb2_grpc.py
@@ -34,6 +34,11 @@ class UserServiceStub(object):
                 request_serializer=user__service__pb2.GetPeerInfoRequest.SerializeToString,
                 response_deserializer=user__service__pb2.GetPeerInfoResponse.FromString,
                 )
+        self.get_friend_stats = channel.unary_unary(
+                '/user_service.UserService/get_friend_stats',
+                request_serializer=user__service__pb2.GetFriendStatsRequest.SerializeToString,
+                response_deserializer=user__service__pb2.GetFriendStatsResponse.FromString,
+                )
 
 
 class UserServiceServicer(object):
@@ -63,6 +68,12 @@ class UserServiceServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def get_friend_stats(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_UserServiceServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -85,6 +96,11 @@ def add_UserServiceServicer_to_server(servicer, server):
                     servicer.get_peer_info,
                     request_deserializer=user__service__pb2.GetPeerInfoRequest.FromString,
                     response_serializer=user__service__pb2.GetPeerInfoResponse.SerializeToString,
+            ),
+            'get_friend_stats': grpc.unary_unary_rpc_method_handler(
+                    servicer.get_friend_stats,
+                    request_deserializer=user__service__pb2.GetFriendStatsRequest.FromString,
+                    response_serializer=user__service__pb2.GetFriendStatsResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -161,5 +177,22 @@ class UserService(object):
         return grpc.experimental.unary_unary(request, target, '/user_service.UserService/get_peer_info',
             user__service__pb2.GetPeerInfoRequest.SerializeToString,
             user__service__pb2.GetPeerInfoResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def get_friend_stats(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/user_service.UserService/get_friend_stats',
+            user__service__pb2.GetFriendStatsRequest.SerializeToString,
+            user__service__pb2.GetFriendStatsResponse.FromString,
             options, channel_credentials,
             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
